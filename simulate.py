674686d0d389f1b1e82f4fcd1c952d9e9834e2d7
Cole Richardson <cricha31@uvm.edu>
2025-01-20 12:24:36 -0500

    Stepping the world to slow it down.

--- simulate.py
+++ simulate.py
@@ -1,6 +1,20 @@
 # -*- coding: utf-8 -*-
+# Cole Richardson
 
+# Imports
 import pybullet as p
+import time
 
+# Connect to the physics engine and open the GUI
 physicsClient = p.connect(p.GUI)
-p.disconnect
+
+# Potential spot to add in things to the simulation later
+
+# Run the simulation for 1000 steps
+for i in range(1000):
+    p.stepSimulation()  # Step the simulation forward
+    time.sleep(1/60)  # Slow down the simulation to ~60 FPS
+    print(i)  # Print the iteration number to track the progress
+
+# Disconnect the physics engine
+p.disconnect()
