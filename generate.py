544debdea270120a91cf2ae15f933db045aea97d
Cole Richardson <cricha31@uvm.edu>
2025-01-27 16:52:18 -0500

    Creating box for simulation.

--- generate.py
+++ generate.py
@@ -1,2 +1,12 @@
 # Import pyrosim
 import pyrosim.pyrosim as pyrosim
+
+# Tell pyrosim the name of the file where the world will be stored
+# Currently called box because it will only contain a box
+pyrosim.Start_SDF("box.sdf")
+
+# Store box position and parameters
+pyrosim.Send_Cube(name="Box", pos=[0,0,0.5] , size=[1,1,1])
+
+# Appending
+pyrosim.End()
