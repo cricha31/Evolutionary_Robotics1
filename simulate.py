6b0a2130c6ea789c57b7dada844a57028b6c286c
Cole Richardson <cricha31@uvm.edu>
2025-01-27 18:03:57 -0500

    Simulating the towers in world.

--- simulate.py
+++ simulate.py
@@ -2,14 +2,23 @@
 # Cole Richardson
 
 # Imports
+import pybullet_data
 import pybullet as p
 import time
 
 # Connect to the physics engine and open the GUI
 physicsClient = p.connect(p.GUI)
+p.setAdditionalSearchPath(pybullet_data.getDataPath())
+
+# Add gravity to simulation
+p.setGravity(0,0,-9.8,physicsClient)
+
+# Add a floor
+planeId = p.loadURDF("plane.urdf")
+
 
 # Simulating a box
-p.loadSDF("box.sdf")
+p.loadSDF("5x5_towers.sdf")
 
 # Run the simulation for 1000 steps
 for i in range(1000):
