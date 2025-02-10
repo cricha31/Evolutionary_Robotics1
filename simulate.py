a7deb41062fe45ac608073cc748876fe9908b9c8
Cole Richardson <cricha31@uvm.edu>
2025-02-10 17:29:05 -0500

    Simulating with front and back leg sensors and saving the data.

--- simulate.py
+++ simulate.py
@@ -5,6 +5,11 @@
 import pybullet_data
 import pybullet as p
 import time
+import numpy
+# Import pyrosim
+import pyrosim.pyrosim as pyrosim
+# Import os
+import os
 
 # Connect to the physics engine and open the GUI
 physicsClient = p.connect(p.GUI)
@@ -22,11 +27,34 @@ robotId = p.loadURDF("body.urdf")
 # Simulating a box
 p.loadSDF("world.sdf")
 
+# Indicating which robot you want prepared for simulation
+pyrosim.Prepare_To_Simulate(robotId)
+
+# Create empty arrays to store data
+backLegSensorValues = numpy.zeros(1000)
+frontLegSensorValues = numpy.zeros(1000)
+
 # Run the simulation for 1000 steps
 for i in range(1000):
     p.stepSimulation()  # Step the simulation forward
+    # create backleg and frontlet touch sensor
+    backLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("BackLeg")
+    frontLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("FrontLeg")
     time.sleep(1/60)  # Slow down the simulation to ~60 FPS
-    print(i)  # Print the iteration number to track the progress
 
 # Disconnect the physics engine
 p.disconnect()
+
+# Print dataset1
+print(backLegSensorValues)
+# Print dataset2
+print(frontLegSensorValues)
+
+# Create the "data" directory if it doesn't exist
+os.makedirs("data", exist_ok=True)
+
+# Save the dataset as a numpy .npy file
+numpy.save("data/backLegSensorValues.npy", backLegSensorValues)
+
+# Save the dataset as a numpy .npy file
+numpy.save("data/frontLegSensorValues.npy", frontLegSensorValues)
