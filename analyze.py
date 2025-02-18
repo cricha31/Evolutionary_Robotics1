7b5459d9f425e296d6a4a0a48cb093b767944679
Cole Richardson <cricha31@uvm.edu>
2025-02-17 20:52:53 -0500

    Graphing the resulting motor values of walking robot.

--- analyze.py
+++ analyze.py
@@ -7,8 +7,12 @@ import matplotlib.pyplot as plt
 backLegSensorValues = np.load("data/backLegSensorValues.npy")
 # Load the second dataset using numpy
 frontLegSensorValues = np.load("data/frontLegSensorValues.npy")
+#Load sinusoidal motor data
+backLegTargetAngles = np.load("data/backLegTargetAngles.npy")
+frontLegTargetAngles = np.load("data/frontLegTargetAngles.npy")
 
-# Print the loaded sensor values
+
+""""# Print the loaded sensor values
 print("Back Leg Sensor Values:", backLegSensorValues)
 print("Front Leg Sensor Values", frontLegSensorValues)
 
@@ -18,6 +22,10 @@ plt.plot(frontLegSensorValues, label="Front")
 plt.title("Front and Back Leg Sensor Values")
 plt.xlabel("Time Step")
 plt.ylabel("Sensor Value")
+plt.legend()"""
+
+plt.plot(backLegTargetAngles, label="Back")
+plt.plot(frontLegTargetAngles, label="Front")
 plt.legend()
 
 # Show the plot
