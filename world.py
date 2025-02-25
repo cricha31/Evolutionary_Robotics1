ce84c8093c64cbbf6c2fcf972d64835e6f918051
Cole Richardson <cricha31@uvm.edu>
2025-02-24 21:37:22 -0500

    Creating new file for world set up.

---
+++ world.py
@@ -0,0 +1,18 @@
+# Creating new file for class world
+
+# Imports
+import pybullet as p
+import pybullet_data
+import constants as c  # Import constants
+
+class WORLD:
+
+    def __init__(self):
+
+        """Loads the world environment including the plane and other objects."""
+        # Load floor plane
+        self.planeId = p.loadURDF("plane.urdf")
+
+        # Load the world with additional objects
+        p.loadSDF("world.sdf")
+
