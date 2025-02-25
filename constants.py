e9ead6d2b2c5e6e13cf515a74151e0d4bc1cd57f
Cole Richardson <cricha31@uvm.edu>
2025-02-24 21:36:50 -0500

    Creating file to store constants as variables.

---
+++ constants.py
@@ -0,0 +1,25 @@
+# File to store values as variables
+
+# Imports
+import numpy
+
+#Double motor controls
+AMPLITUDE = numpy.pi /4
+FREQUENCY = 50
+OFFSET = 0
+
+# Motor control parameters for BackLeg
+AMPLITUDE_BACK_LEG = numpy.pi / 4
+FREQUENCY_BACK_LEG = 10
+PHASE_OFFSET_BACK_LEG = 0
+
+# Motor control parameters for FrontLeg
+AMPLITUDE_FRONT_LEG = numpy.pi / 4
+FREQUENCY_FRONT_LEG = 10
+PHASE_OFFSET_FRONT_LEG = numpy.pi  # Phase shifted by pi
+
+# Simulation parameters
+GRAVITY = -9.8
+TIMESTEPS = 1000
+MAX_FORCE = 100
+FRAME_RATE = 1 / 30  # 60 FPS
