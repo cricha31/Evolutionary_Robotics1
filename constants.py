400b4a39392ba127855e1337d0a11085fe4ee36c
cricha31 <cricha31@uvm.edu>
2025-03-17 11:10:13 -0400

    Shortening simulation

--- constants.py
+++ constants.py
@@ -20,6 +20,6 @@ PHASE_OFFSET_FRONT_LEG = numpy.pi  # Phase shifted by pi
 
 # Simulation parameters
 GRAVITY = -9.8
-TIMESTEPS = 1000
+TIMESTEPS = 100
 MAX_FORCE = 50
 FRAME_RATE = 1 / 30  # 60 FPS
